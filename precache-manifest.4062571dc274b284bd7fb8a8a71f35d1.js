36a296737d6695377a6d0ae27e84b23e85ddbb45
Fitsum <fitbez@gmail.com>
2020-08-24 11:41:33 -0400

    Updates

---
+++ precache-manifest.4062571dc274b284bd7fb8a8a71f35d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8deaaf9b82ac77b31a19b79cd9674728",
+    "url": "/country/index.html"
+  },
+  {
+    "revision": "90da2ab2069e5dde49d6",
+    "url": "/country/static/css/main.b89a7be8.chunk.css"
+  },
+  {
+    "revision": "bf50c43b70b17d8daea9",
+    "url": "/country/static/js/2.0551b1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country/static/js/2.0551b1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90da2ab2069e5dde49d6",
+    "url": "/country/static/js/main.cdc6db2e.chunk.js"
+  },
+  {
+    "revision": "d2ff651bf11cf223e6b0",
+    "url": "/country/static/js/runtime-main.f5e8bae6.js"
+  }
+]);
