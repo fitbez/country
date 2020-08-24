0de562fdedd5b9d8d9b105c364d30fe8c38e85d6
Fitsum <fitbez@gmail.com>
2020-08-24 11:35:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchedCountry, setSearchedCountry] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchedCountry(e.target.value);
+  };
+
+  // console.log(searchedCountry);
+
+  //country to show from the search bar
+  const filteredCountries = searchedCountry.toUpperCase();
+  const countryToShow = countries.filter((country) => {
+    return country.name.toUpperCase().indexOf(filteredCountries) > -1;
+  });
+
+  //fetching data from the api
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      // console.log(response.data);
+      setCountries(response.data);
+    });
+  }, []);
+
+  console.log(countryToShow);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        Search country: <input onChange={handleSearch} type="text" />
+      </div>
+      <div className="show-countries">
+        {countryToShow.length >= 250 ? (
+          <p>Please enter a country to know about....</p>
+        ) : countryToShow.length >= 20 ? (
+          <p>Too many matches, specify another filter</p>
+        ) : countryToShow.length === 1 ? (
+          countryToShow.map((country) => {
+            let pop = parseInt(country.population).toLocaleString();
+            return (
+              <div key={country.name}>
+                <div className="country-header" key={country.name}>
+                  <h2>{country.name}</h2>
+                  <div className="flag-image">
+                    <img src={country.flag} alt="flag" />
+                  </div>
+                </div>
+                <div className="country-info">
+                  <div>
+                    <p>
+                      Capital:{" "}
+                      <span className="capital">{country.capital}</span>{" "}
+                    </p>
+                    <p>
+                      Population: <span className="population">{pop}</span>{" "}
+                    </p>
+                  </div>
+                  <div className="language">
+                    <h3>Languages</h3>
+                    <div>
+                      {country.languages.map((language) => {
+                        return <li key={language.name}>{language.name}</li>;
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          countryToShow.map((country) => {
+            return searchedCountry.length ? (
+              <div className="country-list" key={country.name}>
+                <span onClick={() => setSearchedCountry(country.name)}>
+                  {country.name}
+                </span>
+              </div>
+            ) : (
+              <div></div>
+            );
+          })
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/countries.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Weather = () => {
+  return <h2>Hello countries</h2>;
+};
+
+export default Weather;
